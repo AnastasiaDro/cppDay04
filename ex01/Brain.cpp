e04514d2303c8bca106ede1e7d349d64f773c5eb
cerebus <cerebus@student.21-school.ru>
2021-10-03 19:42:33 +0300

    started make some Brain class stuff

---
+++ ex01/Brain.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Cesar Erebus on 10/3/21.
+//
+
+#include "Brain.hpp"
---
+++ ex01/Dog.cpp
@@ -0,0 +1,33 @@
+//
+// Created by Cesar Erebus on 10/3/21.
+//
+
+#include "Dog.hpp"
+
+Dog::Dog()
+{
+	_type = "Dog";
+	printMsg("DOG Default Constructor here");
+}
+
+//Copy constructor
+Dog::Dog(const Dog &orig)
+{
+	*this = orig;
+	printMsg("DOG Copy Constructor here");
+}
+
+//Desrtructor
+Dog::~Dog(){
+	printMsg("DOG Destructor here");
+}
+
+Dog &Dog::operator=(const Dog &orig) {
+	this->_type = orig.getType();
+	return *this;
+}
+
+void  Dog::makeSound() const{
+	printMsg(this->_type + " says: \"Woof-woof!\"");
+}
+

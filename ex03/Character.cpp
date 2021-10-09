92581c9ac5705973d896c13564b0f6d72098fe0c
AnastasiaDro <adrogunova@gmail.com>
2021-10-09 16:30:03 +0300

    -added realisation of the Character class

--- ex03/Character.cpp
+++ ex03/Character.cpp
@@ -3,3 +3,38 @@
 //
 
 #include "Character.hpp"
+
+Character::Character() {
+    this->_name = "NoName";
+    capacity = materNum;
+    materias = new AMateria *[materNum];
+}
+
+Character::Character(std::string name) {
+    this->_name = name;
+    capacity = materNum;
+    materias = new AMateria *[materNum];
+}
+
+Character::~Character() {
+    delete materias;
+}
+
+std::string const &Character::getName() const {
+    return this->_name;
+}
+
+void Character::equip(AMateria *m) {
+    if (this->capacity == 0 || !m)
+        return ;
+    this->materias[materNum - this->capacity] = m; 
+}
+
+void Character::unequip(int idx)
+{
+    if (idx < 0 || idx >= this->materNum)
+        return;
+    this->materias[idx] = nullptr;
+}
+
+
--- ex03/Character.hpp
+++ ex03/Character.hpp
@@ -7,13 +7,22 @@
 
 
 #include "ICharacter.hpp"
+#include <string>
 
 class Character : public ICharacter {
 private:
 	std::string _name;
+	AMateria **materias;
+	static const int materNum = 4;
+    int capacity;
+    Character();
 public:
-	Character();
+    Character(std::string name);
 	virtual ~Character();
+    virtual std::string const & getName() const;
+    virtual void equip(AMateria* m);
+    virtual void unequip(int idx);
+    virtual void use(int idx, ICharacter& target);
 };
 
 
